cbdf11fa9ff767aad90317fb8a15579144031b31
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-02-16 14:05:32 +0500

    lodash

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+let input = "  JavaScript  ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = (str) => str.trim();
+const wrap = (type, str) => `<span>${str}</span>`
+const toLowerCase = (str) => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+
+console.log(transform(input));
+

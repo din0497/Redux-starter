36a6efb7960a88b9f34cfe0888cb15096c7abd9d
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-02-16 14:44:56 +0500

    mutibility

--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ let input = "  JavaScript  ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrap = (type, str) => `<span>${str}</span>`
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-const transform = pipe(trim, toLowerCase, wrapInDiv);
+const transform = pipe(trim, toLowerCase, wrap("div"));
 
 console.log(transform(input));
 
+const person = {
+  name: "Asad",
+  adress: {
+    country: "usa",
+    city: "san fran",
+  },
+};
+
+const updated = {
+  ...person,
+  adress: {
+    ...person.adress,
+    city:'New York'
+  },
+  name: "Din",
+};
+updated.adress.city = "New York";
+console.log(updated, person);
